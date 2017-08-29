f390b4cd72b5c4237837f352e007009f9af1a1ea
IsaacSec <isaacsecundino3@gmail.com>
2017-08-29 19:04:49 +0000

    First commit and Firts activity design

---
+++ app/src/main/java/api/FlickrCall.java
@@ -0,0 +1,9 @@
+package api;
+
+/**
+ * Created by isaac on 8/29/17.
+ */
+
+public class FlickrCall {
+
+}
---
+++ app/src/main/java/api/HttpClient.java
@@ -0,0 +1,26 @@
+package api;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Created by isaac on 8/29/17.
+ */
+
+public class HttpClient {
+
+    private OkHttpClient client = new OkHttpClient();
+
+    String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+}
